8f3ed788066bd65e674f99572c87cc25aaee8d81
yikx11 <1019590642@qq.com>
2023-06-12 18:32:09 +0800

    init

---
+++ community/src/main/java/com/nowcoder/community/CommunityApplication.java
@@ -0,0 +1,16 @@
+package com.nowcoder.community;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication()
+@MapperScan(basePackages = "com.nowcoder.community.dao")
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
